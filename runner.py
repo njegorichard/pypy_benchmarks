f33ded3675dc38e8ad30175acfe624ed760468ca
Maciej Fijalkowski <fijall@gmail.com>
2010-01-15 20:01:50 +0000

    Scrap the need of pylib installed

--- runner.py
+++ runner.py
@@ -2,7 +2,7 @@
 """ Usage: runner.py <result filename> <path to pypy-c>
 """
 
-import py
+import os
 import json
 import sys
 from unladen_swallow.perf import main
@@ -10,10 +10,10 @@ from unladen_swallow.perf import main
 def run_and_store(benchmarks, result_filename, pypy_c_path):
     results = main(['-f', '-b', ','.join(benchmarks),
                     '--no_charts', sys.executable, pypy_c_path])
-    py.path.local(result_filename).write(
-        json.dumps([(name, result.__class__.__name__, result.__dict__)
-                    for name, result in results])
-    )
+    f = open(str(result_filename), "w")
+    f.write(json.dumps([(name, result.__class__.__name__, result.__dict__)
+                    for name, result in results]))
+    f.close()
 
 if __name__ == '__main__':
     BENCHMARK_SET = ['richards', 'slowspitfire', 'django', 'spambayes',
