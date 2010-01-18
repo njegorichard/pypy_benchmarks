af44b4a817adc12f33e3f1fe9226f4d77467ad65
Maciej Fijalkowski <fijall@gmail.com>
2010-01-18 12:46:13 +0000

    kill pylib usage, it's for buildslaves after all, should be as standalone as possible

--- benchmarks.py
+++ benchmarks.py
@@ -1,9 +1,10 @@
 
-import py
+import os
 from unladen_swallow.perf import SimpleBenchmark, MeasureGeneric
 
 def MeasureFloat(python, options):
-    bm_path = str(py.path.local(__file__).dirpath().join("shootout", "float.py"))
+    bm_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                           'shootout', 'float.py')
     return MeasureGeneric(python, options, bm_path)
 
 def BM_float(*args, **kwds):
