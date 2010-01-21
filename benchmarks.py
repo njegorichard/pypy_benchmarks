9c2350915e8bf9447a73dd88169311f8240bac3e
Maciej Fijalkowski <fijall@gmail.com>
2010-01-21 21:26:40 +0000

    Add gcbench. Mostly for the joy of merging direct-assembler-call branch ;-)

--- benchmarks.py
+++ benchmarks.py
@@ -5,10 +5,10 @@ from unladen_swallow.perf import SimpleBenchmark, MeasureGeneric
 def relative(*args):
     return os.path.join(os.path.dirname(os.path.abspath(__file__)), *args)
 
-def _register_new_bm(name, d):
+def _register_new_bm(name, d, **opts):
     def Measure(python, options):
         bm_path = relative('own', name + '.py')
-        return MeasureGeneric(python, options, bm_path)
+        return MeasureGeneric(python, options, bm_path, **opts)
     Measure.func_name = 'Measure' + name.capitalize()
 
     def BM(*args, **kwds):
@@ -17,6 +17,10 @@ def _register_new_bm(name, d):
 
     d[BM.func_name] = BM
 
+opts = {
+    'gcbench' : {'iteration_scaling' : .10},
+}
+
 for name in ['float', 'nbody_modified', 'meteor-contest', 'fannkuch',
-             'spectral-norm', 'chaos', 'telco']:
-    _register_new_bm(name, globals())
+             'spectral-norm', 'chaos', 'telco', 'gcbench']:
+    _register_new_bm(name, globals(), **opts.get(name, {}))
