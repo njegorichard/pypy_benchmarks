a20a614df2ffdf5f818d04eb1277d2244973fc23
Antonio Cuni <anto.cuni@gmail.com>
2011-07-16 10:27:54 +0200

    kill the symlink, and use a python hack to share the content of util.py in both directories

--- own/util.py
+++ own/util.py
@@ -1,1 +1,5 @@
-../unladen_swallow/performance/util.py
+import os.path
+
+root = os.path.abspath(os.path.join(__file__, '..', '..'))
+util_py = os.path.join(root, 'unladen_swallow', 'performance', 'util.py')
+execfile(util_py)
