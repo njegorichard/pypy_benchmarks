1ebfba235eba84d08c235bf42f2fd11949cc0b98
Maciej Fijalkowski <fijall@gmail.com>
2015-02-21 18:03:55 +0200

    add graph alloc removal bench

---
+++ warmup/pypy-graph-alloc-removal.py
@@ -0,0 +1,43 @@
+from rpython.translator.interactive import Translation
+from rpython.translator.translator import graphof
+from rpython.translator.backendopt.malloc import LLTypeMallocRemover
+from rpython.flowspace.model import copygraph
+
+import time
+
+class X(object):
+    pass
+
+def f(n):
+    x = X()
+    x.attr = n
+    y = X()
+    y.attr = 6 if n > 100 else 5
+    return x.attr + y.attr
+
+
+t = Translation(f, [int])
+t.annotate()
+t.rtype()
+
+graph = graphof(t.context, f)
+
+def main(graph, repeat):
+    start = time.time()
+    l = []
+    for i in range(repeat):
+        print i
+        l.append(f(graph))
+    print l
+    end = time.time()
+    print end - start
+
+def f(graph):
+    start = time.time()
+    for k in range(10):
+        g = copygraph(graph)
+        remover = LLTypeMallocRemover()
+        remover.remove_mallocs_once(g)
+    return time.time() - start
+
+main(graph, 500)
